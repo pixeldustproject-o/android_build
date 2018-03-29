480000b6b7410ac1a677924de15c5811d73cf06b
Calin Juravle <calin@google.com>
2018-03-28 18:31:00 -0700

    Change compiler filter for install to speed-profile
    The install filter is speed-profile in order to enable the use of profiles
    from the dex metadata files. Note that if a profile is not provided or if
    it is empty speed-profile is equivalent to (quicken + empty app image).
    
    Test: build & install an app
    Bug: b/30934496
    Change-Id: I895b5e2a1d8ff478f56b0f08cb589789d0743b54
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- target/product/runtime_libart.mk
+++ target/product/runtime_libart.mk
@@ -67,11 +67,15 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     dalvik.vm.dexopt.secondary=true \
     dalvik.vm.appimageformat=lz4
 
-# make "boot" reasons only extract for faster turnaround.
-
+# Make "boot" reasons only extract for faster turnaround.
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     pm.dexopt.first-boot=extract \
-    pm.dexopt.boot=verify \
+    pm.dexopt.boot=extract \
+
+# The install filter is speed-profile in order to enable the use of
+# profiles from the dex metadata files. Note that if a profile is not provided
+# or if it is empty speed-profile is equivalent to (quicken + empty app image).
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     pm.dexopt.install=speed-profile \
     pm.dexopt.bg-dexopt=speed-profile \
     pm.dexopt.ab-ota=speed-profile \
