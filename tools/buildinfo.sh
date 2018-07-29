5c1557216dca9da74540ce63d004c99b19716d27
spezi77 <spezi7713@gmx.net>
2018-07-29 12:21:08 +0200

    Revert "Generate properties for PixeldustOTA."
    This reverts commit b02dd6b004062a4a18154b006315f95979d4d4e8.
    
    Moved to vendor/pixeldust.

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -31,9 +31,6 @@ echo "ro.product.brand=$PRODUCT_BRAND"
 echo "ro.product.name=$PRODUCT_NAME"
 echo "ro.product.device=$TARGET_DEVICE"
 
-echo "ro.pixeldust.device=$TARGET_DEVICE"
-echo "ro.pixeldust.ota.version=pixeldust_$TARGET_DEVICE-$PIXELDUST_VERSION"
-
 # These values are deprecated, use "ro.product.cpu.abilist"
 # instead (see below).
 echo "# ro.product.cpu.abi and ro.product.cpu.abi2 are obsolete,"
